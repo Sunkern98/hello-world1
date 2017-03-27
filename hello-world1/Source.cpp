5f758c09e9fd4780c441b14bf1c9bb12eac605ca
Scott <sloth.13@hotmail.com>
2017-03-27 11:04:48 +1030

    Differentchange

--- hello-world1/Source.cpp
+++ hello-world1/Source.cpp
@@ -3,6 +3,8 @@ using namespace std;
 void main()
 {
 	char something;
-	cout << "hello world!";
+	int somethingDifferent = 3;
+	for(int i = 0; i < somethingDifferent; i++)
+	cout << "hello world!\n";
 	cin >> something;
 }
